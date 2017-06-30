c760da5e18fc8e3997cf52d74c5c565e7cceb46b
zhangxx <liaisonme@hotmail.com>
2017-06-30 12:08:21 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,62 @@
+class Program {
+    /// <summary>
+    /// an tiny arithmetic-operation function by QQ:20437023
+    /// MIT License Copyright (c) 2017 zhangxx2015
+    /// 一个四则运算函数,支持运算符优先级 仅 20 行代码, 
+    /// 实现了:
+    ///     加法(+),
+    ///     减法(-),
+    ///     乘法(*),
+    ///     除法(/),
+    ///     取余(%),
+    /// 
+    /// an arithmetic-operation function,support PRI., only 20 lines of code,
+    /// Implement features:
+    ///     addition      (+),
+    ///     subduction    (-),
+    ///     multiplication(*),
+    //      division      (/),
+    //      remainder     (%)
+    /// </summary>
+    private static double Eval(string express) {
+        while (true) {
+            var lb = express.LastIndexOf('(');
+            if (lb == -1) break;
+            var rb = express.IndexOf(')', lb);
+            if (-1 == rb) throw new System.Exception("Symbol is not match!");
+            express = express.Substring(0, lb) + Eval(express.Substring(1 + lb, rb - lb - 1)) + express.Substring(rb + 1);
+        }
+        double ret;
+        if (double.TryParse(express, out ret)) return ret;
+        var op = express.IndexOf('+');
+        if (-1 != op) return Eval(express.Substring(0, op)) + Eval(express.Substring(1 + op));
+        op = express.IndexOf('-');
+        if (-1 != op) return Eval(express.Substring(0, op)) - Eval(express.Substring(1 + op));
+        op = express.IndexOf('*');
+        if (-1 != op) return Eval(express.Substring(0, op)) * Eval(express.Substring(1 + op));
+        op = express.IndexOf('/');
+        if (-1 != op) return Eval(express.Substring(0, op)) / Eval(express.Substring(1 + op));
+        throw new System.Exception("Unknow error!");
+    }
+    // Examples
+    static void Main() {
+        var ret1 = -11 + (1.2 + 3.4)*5.6/((7.8 + 9) - 10);
+        var ret2 = Eval("-11 + (1.2 + 3.4) * 5.6 / ((7.8 + 9) - 10)");
+        System.Console.WriteLine("-11 + (1.2 + 3.4) * 5.6 / ((7.8 + 9) - 10) = {0},validate:{1}", ret1, ret1 == ret2);
+        // benchmark
+        var myTicks = System.Environment.TickCount;
+        for (var i = 0; i < 1000000; i++) {
+            var ret = Eval("-11 + (1.2 + 3.4) * 5.6 / ((7.8 + 9) - 10)");
+        }
+        myTicks = System.Environment.TickCount - myTicks;
+
+        var sysTicks = System.Environment.TickCount;
+        for (var i = 0; i < 1000000; i++) {
+            var ret = -11 + (1.2 + 3.4) * 5.6 / ((7.8 + 9) - 10);
+        }
+        sysTicks = System.Environment.TickCount - sysTicks;
+        
+        System.Console.WriteLine("execute 1,000,000 times,eval function elapse:{0} ms,native code elapse:{1} ms", myTicks, sysTicks);
+        System.Console.ReadLine();
+    }
+}
